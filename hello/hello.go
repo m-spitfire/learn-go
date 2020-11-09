860322d72c5b399d5088a3b74a2f2b4da293a6f9
spitfire-hash <carlsonmu@gmail.com>
2020-11-09 22:31:56 +0400

    Added Hellos function to greetings package
    The function returns a map that contains multiple
    greetings for multiple people.

--- hello/hello.go
+++ hello/hello.go
@@ -13,9 +13,13 @@ func main() {
     // the log entry prefix and a flag to disable printing
     // the time, source file, and line number.
     log.SetPrefix("greetings: ")
-    log.SetFlags(0)
-    // Get a greeting message and print it.
-	message, err := greetings.Hello("Gladys")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	
+	// Get a greeting message and print it.
+	message, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
