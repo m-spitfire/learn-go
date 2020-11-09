7472dae43ec4fd4620f2437370332b31ec54f8da
spitfire-hash <carlsonmu@gmail.com>
2020-11-09 22:27:43 +0400

    added error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 
 func main() {
+	// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
     // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("Gladys")
+	// If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+	}
+	// If no error returned, print returned message to console
     fmt.Println(message)
 }
